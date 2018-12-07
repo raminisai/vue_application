743c357b9458524ca7976eb6d2f271262b5d4f01
sai krishna <raminisai33@gmail.com>
2018-12-07 15:28:06 +0530

    sample---post

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
       <label>Blog Title:</label>
       <input type="text" v-model.lazy="blog.title" required />
       <label>Blog Content:</label>
       <textarea v-model.lazy.trim="blog.content"></textarea>
+      <div id="checkboxes">
+        <p>Blog Categories:</p>
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories" />
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories" />
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories" />
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories" />
+      </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{ author }}</option>
+      </select>
+      <hr />
+      <button v-on:click="post">Add Blog</button>
     </form>
+    <div v-if="submitted"><h3>Thanks for adding your post</h3></div>
     <div id="preview">
       <h3>Preview blog</h3>
       <p>Blog title: {{ blog.title }}</p>
       <p>Blog content:</p>
       <p style="white-space: pre">{{ blog.content }}</p>
+      <p>Blog Categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
+      <p>Author: {{ blog.author }}</p>
     </div>
   </div>
 </template>
@@ -23,11 +46,28 @@ export default {
     return {
       blog: {
         title: "",
-        content: ""
-      }
+        content: "",
+        categories: [],
+        author: ""
+      },
+      authors: ["The Net Ninja", "The Angular Avenger", "The Vue Vindicator"],
+      submitted: false
     };
   },
-  methods: {}
+  methods: {
+    post: function() {
+      this.$http
+        .post("http://jsonplaceholder.typicode.com/posts", {
+          title: this.blog.title,
+          body: this.blog.content,
+          userId: 1
+        })
+        .then(function(data) {
+          console.log(data);
+          this.submitted = true;
+        });
+    }
+  }
 };
 </script>
 
@@ -56,5 +96,13 @@ textarea {
 }
 h3 {
   margin-top: 10px;
+}
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkboxes label {
+  display: inline-block;
+  margin-top: 0;
 }
 </style>
